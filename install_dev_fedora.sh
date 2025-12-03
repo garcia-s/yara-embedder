2a093c32bfbd4cd8691baf762fb37a95fb0efd12
Juan García <43949658+garcia-s@users.noreply.github.com>
2025-12-02 23:59:42 -0500

    Creating the folder in the install script and ignoring the fk out of that folder

--- install_dev_fedora.sh
+++ install_dev_fedora.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-
-
-
-
 if ! command -v "flutter" >/dev/null 2>&1; then
     echo 'You need to have flutter installed';
     exit 1;
@@ -30,6 +26,8 @@ flutter_hash=$(flutter --version --machine | jq -r '.engineContentHash')
 curl "https://storage.googleapis.com/flutter_infra_release/flutter/$flutter_hash/linux-x64/linux-x64-embedder.zip" -o  /tmp/linux-x64-embedder.zip
 
 unzip /tmp/linux-x64-embedder.zip -d /tmp
+
+mkdir ./engine
 mv /tmp/flutter_embedder.h ./engine
 mv /tmp/libflutter_engine.so ./engine
 
